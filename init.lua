bdb645810f7a82031e934713aff6d2b417ce6bdf
Nathan Marley <nathan@glowberrylabs.com>
2026-08-06 14:30:53 +0800

    Disable mouse, keep hitting accidentally

--- init.lua
+++ init.lua
@@ -113,7 +113,7 @@ do
   vim.o.relativenumber = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
-  vim.o.mouse = 'a'
+  -- vim.o.mouse = 'a'
 
   -- Don't show the mode, since it's already in the status line
   vim.o.showmode = false
